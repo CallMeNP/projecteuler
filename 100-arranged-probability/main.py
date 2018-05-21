0acf718f97c8c257215752dcae7e5ffaa00294dd
hailong.gao <np.liamg@gmail.com>
2018-05-21 16:46:15 +0800

    100

---
+++ 100-arranged-probability/main.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2018 Hailong Gao <np.liamg@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+蓝球为a，全部为A
+# 从wolfram alpha 输入 2A^2 - 2A + 1 = k^2
+# 得到 A的整数通解 (Integer solutions)
+输入通解>10^12
+求出n=-16 ;求出A=1070379110497;a=756872327473
+
+# 所以本题本质上是求丢番图方程的整数解
+"""
+# todo 丢番图方程通解的编程求法
